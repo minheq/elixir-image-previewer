f6b11f328b6fc1ecaf6af29e60b992836428f914
Vu Hoang Minh <vuhminh@gmail.com>
2017-04-23 14:00:15 +0800

    Add heuristics to getting image, and handle some edge cases

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # ImagePreviewer
 
-**TODO: Add description**
+### Install dependencies
+```
+mix deps.get
+```
+
+### Run the code
+```
+mix deps.get
+```
+
+### Run the code
+*POST* /image-preview with params containing *url* key
+
+```
+POST /image-preview
+# { url: https://www.lazada.sg/ }
+# { url: https://elixirschool.com/ }
+# { url: http://elixir-lang.org/ }
+# { url: https://github.com/devinus/poison/ }
+# { url: http://cuzzey.com/ }
+# { url: https://hexdocs.pm/elixir/Kernel.html }
+```
--- lib/image_previewer/parser.ex
+++ lib/image_previewer/parser.ex
@@ -23,7 +23,8 @@ defmodule ImagePreviewer.Parser do
   def get_preview(conn) do
     preview = conn.body_params["url"]
       |> find_tags_from_url
-      |> get_preview_properties
+      # we pass url as second argument in case the image url is relative e.g. "/path/to/img.jpg"
+      |> get_preview_properties(conn.body_params["url"])
 
     conn
       |> put_resp_content_type("application/json")
@@ -39,13 +40,13 @@ defmodule ImagePreviewer.Parser do
         raise message: "Not found", plug_status: 400
       {:error, %HTTPoison.Error{reason: reason}} ->
         IO.puts reason
-        raise message: reason, plug_status: 400
+        raise Exception
       _ ->
         raise Exception
     end
   end
 
-  defp get_preview_properties(tags) do
+  defp get_preview_properties(tags, url) do
     # Prioritize getting image, title, description from og attributes
     # performance and readibility. Opted for readibility for people reading the code
     # Find og tags first, if there are og tags, we return tags with og values
@@ -56,7 +57,7 @@ defmodule ImagePreviewer.Parser do
         filtered_tags
           |> get_values_from_og_attributes
     # If og attributes not found, use standard <img> and <title>
-      {:standard} -> get_values_from_standard_attr(tags)
+      {:standard} -> get_values_from_standard_attr(tags, url)
       _ -> %{}
     end
   end
@@ -84,13 +85,18 @@ defmodule ImagePreviewer.Parser do
     end
   end
 
-  def get_values_from_standard_attr(tags) do
+  def get_values_from_standard_attr(tags, url) do
     lookup_standard_tags = ["title", "img"]
 
     filtered_tags = tags
                   # Filter out only <img> ang <title> tags
                   |> Enum.filter(&(Enum.member?(lookup_standard_tags, elem(&1, 0))))
+    
+
+    # if filtered_tags do
       
+    # end
+
     image_url = filtered_tags
                 # Get only items that has non-empty "src" for <img> tags
                 |> Enum.filter(
@@ -110,6 +116,8 @@ defmodule ImagePreviewer.Parser do
                       |> Enum.any?
                   end
                 )
+                # Check if there are valid <img> otherwise throw
+                |> throw_if_empty
                 # Get the first image
                 |> List.first
                 # Get the attributes list
@@ -118,7 +126,6 @@ defmodule ImagePreviewer.Parser do
                 |> Enum.filter(
                   fn attribute ->
                     case attribute do
-                      {"src", ""} -> false
                       {"src", value} -> true
                       _ -> false
                     end
@@ -128,12 +135,23 @@ defmodule ImagePreviewer.Parser do
                 |> List.first
                 # Get the image url
                 |> elem(1)
+                # Append url if it has relative path
+                |> (
+                  fn image_url ->
+                    uri = URI.parse(image_url)
+                    cond do
+                      !uri.host -> "#{url}#{image_url}"
+                      true -> image_url
+                    end
+                  end
+                  ).()
       
     title = filtered_tags
             # Get the first tag which is by assumption a <title> tag
             |> List.first
             # Gets its array of children nodes
             |> elem(2)
+            |> throw_if_empty
             # Get the only child text which is the title
             |> List.first
 
@@ -147,6 +165,7 @@ defmodule ImagePreviewer.Parser do
   """
   def get_values_from_og_attributes(tags) do
     tags
+      # Map into arrays of tuples
       |> Enum.map(
         # { tag_name, attributes, children_nodes }
         fn tag -> 
@@ -157,9 +176,9 @@ defmodule ImagePreviewer.Parser do
                     |> Enum.filter(&(
                       elem(&1, 0) == "property" 
                       || elem(&1, 0) == "content"))
-
-          property = List.first(attributes)
-          value = List.last(attributes)
+          
+          property = List.keyfind(attributes, "property", 0)
+          value = List.keyfind(attributes, "content", 0)
 
           case property do
             {"property", "og:title"} ->
@@ -171,6 +190,15 @@ defmodule ImagePreviewer.Parser do
           end
         end
       )
+      # Convert into a Map
       |> Enum.into(%{})
+  end
+
+  defp throw_if_empty (list) do
+    cond do
+      # Throw if there are no images fit
+      Enum.empty?(list) -> raise Exception
+      true -> list
+    end
   end
 end
